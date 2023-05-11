e79edbb4d31b8d6952517abd83b9d9424815d3d0
Yaroslav <yaroslav.bailo@gmail.com>
2023-05-11 18:46:06 +0300

    -[add]- edit profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ProfileEditFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +17,27 @@ class ProfileController extends AbstractController
     public function index(): Response
     {
         return $this->render('main/profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
         ]);
+    }
+
+    /**
+     * @param  Request                 $request
+     * @param  EntityManagerInterface  $entityManager
+     * @Route("/profile/edit", name="app_profile_edit")
+     * @return Response
+     */
+    public function edit(Request $request,EntityManagerInterface $entityManager):Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(ProfileEditFormType::class,$user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+          $entityManager->persist($user);
+          $entityManager->flush();
+          return $this->redirectToRoute('app_profile');
+        }
+        return $this->render('main/profile/edit.html.twig',
+                             ['form'=>$form->createView()]);
+
     }
 }
