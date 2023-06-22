d6a18c8fefe98e63efacc53b21ba874518709a3b
Yaroslav <yaroslav.bailo@gmail.com>
2023-06-22 21:00:07 +0300

    -[add]- file upload handler -[add]- ProductImage entity

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-use App\Form\EditFormProductType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,47 +28,4 @@ class DefaultController extends AbstractController
         );
     }
 
-    /**
-     * Edit product data
-     * @Route("/edit-product/{id}", name="edit_product", methods="GET|POST",requirements={"id"="\d+"})
-     * @Route("/add-product", name="add_product", methods="GET|POST")
-     *
-     * @param  Request  $request
-     * @param  ManagerRegistry  $doctrine
-     * @param  int|null  $id
-     *
-     * @return Response
-     */
-    public function editProduct(
-        Request $request,
-        ManagerRegistry $doctrine,
-        int $id = null
-    ): Response {
-        $entityManager = $doctrine->getManager();
-        if ($id) {
-            $product = $entityManager
-                ->getRepository(Product::class)
-                ->find($id);
-        } else {
-            $product = new Product();
-        }
-        $form = $this->createForm(EditFormProductType::class, $product);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-            $entityManager->persist($product);
-            $entityManager->flush();
-
-            return $this->redirectToRoute(
-                'edit_product',
-                ['id' => $product->getId()]
-            );
-        }
-
-
-        return $this->render(
-            'main/default/edit_product.html.twig',
-            ['form' => $form->createView()]
-        );
-    }
 }
--- src/Form/EditFormProductType.php
+++ src/Form/EditFormProductType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -12,38 +15,102 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EditFormProductType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    public function buildForm(
+        FormBuilderInterface $builder,
+        array $options
+    ): void {
         $builder
-            ->add('title',TextType::class,
-                  [
-                      'label'=>'Title (from class)',
-                      'required'=>true,
-                      'constraints'=>[
-                          new NotBlank()
-                      ]
+            ->add(
+                'title',
+                TextType::class,
+                [
+                    'label'       => 'Title',
+                    'required'    => true,
+                    'attr'        => ['class' => 'form-control'],
+                    'constraints' => [
+                        new NotBlank(),
+                    ],
 
-                  ])
-            ->add('price',NumberType::class,
-                  [
-                      'scale'=> 2,
-                      'html5' =>true,
-                      'attr'=>[
-                         'step'=>0.01
-                      ]
+                ]
+            )
+            ->add(
+                'price',
+                NumberType::class,
+                [
+                    'scale'    => 2,
+                    'html5'    => true,
+                    'required' => true,
+                    'attr'     => [
+                        'class' => 'form-control',
+                        'min'   => 0,
+                        'step'  => 0.01,
+                    ],
 
-                  ])
-            ->add('quantity')
-            ->add('description')
-            ->add('isPublished')
-            ->add('isDeleted')
-        ;
+                ]
+            )
+            ->add(
+                'quantity',
+                IntegerType::class,
+                [
+                    'label'    => 'Quantity',
+                    'required' => true,
+
+                ]
+            )
+            ->add(
+                'description',
+                TextType::class,
+                [
+                    'label'    => 'Description',
+                    'required' => true,
+                    'attr'     => [
+                        'class' => 'form-control',
+                        'style' => 'overflow:hidden',
+                    ],
+                ]
+            )
+            ->add(
+                'newImage',
+                FileType::class,
+                [
+                    'label'    => 'Choose image',
+                    'mapped'   => false,
+                    'required' => false,
+                    'attr'     => ['class' => 'form-control-file'],
+                ]
+            )
+            ->add(
+                'isPublished',
+                CheckboxType::class,
+                [
+                    'label'      => 'is Published',
+                    'required'   => false,
+                    'attr'       => ['class' => 'form-check-input'],
+                    'label_attr' => [
+                        'class' => 'form-check-label',
+                    ],
+                ]
+            )
+            ->add(
+                'isDeleted',
+                CheckboxType::class,
+                [
+                    'label'      => 'is Deleted',
+                    'required'   => false,
+                    'attr'       => ['class' => 'form-check-input'],
+                    'label_attr' => [
+                        'class' => 'form-check-label',
+                    ],
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => Product::class,
-        ]);
+        $resolver->setDefaults(
+            [
+                'data_class' => Product::class,
+            ]
+        );
     }
 }
