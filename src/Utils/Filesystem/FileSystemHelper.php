ed7ef76fe29db76632f49130b5f62125462791f7
Yaroslav Bailo <yaroslav.bailo@gmail.com>
2026-02-17 20:27:37 +0000

    - add some updates

--- src/Utils/Filesystem/FileSystemHelper.php
+++ src/Utils/Filesystem/FileSystemHelper.php
@@ -1,45 +1,27 @@
 <?php
 
-
 namespace App\Utils\Filesystem;
-
 
 use Symfony\Component\Filesystem\Filesystem;
 
 class FileSystemHelper
 {
-    /**
-     * @var Filesystem
-     */
-    private Filesystem $fileSystem;
-
-    /**
-     * FileSystemHelper constructor.
-     */
-    public function __construct(Filesystem $fileSystem)
+    public function __construct(private Filesystem $fileSystem)
     {
-        $this->fileSystem = $fileSystem;
     }
 
-    /**
-     * @param  string  $folder
-     */
     public function createFolder(string $folder): void
     {
-        if ( ! $this->fileSystem->exists($folder)) {
+        if (!$this->fileSystem->exists($folder)) {
             $this->fileSystem->mkdir($folder);
         }
     }
 
-    /**
-     * @param  string  $item
-     */
     public function remove(string $item): void
     {
         if ($this->fileSystem->exists($item)) {
             $this->fileSystem->remove($item);
         }
     }
-
 
 }
