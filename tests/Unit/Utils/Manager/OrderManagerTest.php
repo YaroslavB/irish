83838e43bcf0321182082c89d1941758ac7bc1b9
Yaroslav Bailo <yaroslav.bailo@gmail.com>
2026-04-05 20:06:37 +0000

    feat: add tests for SendOrderConfirmationEmailHandler and update OrderManager for email dispatch

--- tests/Unit/Utils/Manager/OrderManagerTest.php
+++ tests/Unit/Utils/Manager/OrderManagerTest.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Tests\Unit\Utils\Manager;
 
 use App\Entity\Order;
+use App\Message\SendOrderConfirmationEmail;
 use App\Utils\Manager\CartManager;
 use App\Utils\Manager\OrderManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectRepository;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class OrderManagerTest extends TestCase
 {
@@ -18,19 +21,21 @@ class OrderManagerTest extends TestCase
     private MockObject&EntityManagerInterface $entityManager;
     private MockObject&CartManager $cartManager;
     private MockObject&ObjectRepository $repository;
+    private MockObject&MessageBusInterface $bus;
 
     protected function setUp(): void
     {
         $this->entityManager = $this->createMock(EntityManagerInterface::class);
         $this->cartManager = $this->createMock(CartManager::class);
         $this->repository = $this->createMock(ObjectRepository::class);
+        $this->bus = $this->createMock(MessageBusInterface::class);
 
         $this->entityManager
             ->method('getRepository')
             ->with(Order::class)
             ->willReturn($this->repository);
 
-        $this->orderManager = new OrderManager($this->entityManager, $this->cartManager);
+        $this->orderManager = new OrderManager($this->entityManager, $this->cartManager, $this->bus);
     }
 
     public function testGetRepositoryReturnsOrderRepository(): void
@@ -89,6 +94,33 @@ class OrderManagerTest extends TestCase
         $this->entityManager->expects($this->once())->method('flush');
 
         $this->orderManager->save($order);
+    }
+
+    public function testCreateOrderFromCartFromSessionDispatchesConfirmationEmail(): void
+    {
+        $cartRepository = $this->createMock(ObjectRepository::class);
+        $this->cartManager->method('getRepository')->willReturn($cartRepository);
+
+        $cart = $this->createMock(\App\Entity\Cart::class);
+        $cart->method('getCartProducts')->willReturn(new \Doctrine\Common\Collections\ArrayCollection());
+
+        $cartRepository
+            ->method('findOneBy')
+            ->with(['sessionId' => 'abc'])
+            ->willReturn($cart);
+
+        $order = new Order();
+        $this->entityManager->method('persist');
+        $this->entityManager->method('flush');
+
+        $this->bus
+            ->expects($this->once())
+            ->method('dispatch')
+            ->with($this->isInstanceOf(SendOrderConfirmationEmail::class))
+            ->willReturn(new Envelope(new SendOrderConfirmationEmail(0)));
+
+        $user = $this->createMock(\App\Entity\User::class);
+        $this->orderManager->createOrderFromCartFromSession('abc', $user);
     }
 }
 
